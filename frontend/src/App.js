0f81f3cffb26abc5add42f1250cf4816879a18ac
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-03-21 15:21:04 -0400

    nav-bar added and styling for nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,23 @@ import './App.css';
 import AuthPage from './pages/auth';
 import BookingsPage from './pages/booking';
 import EventsPage from './pages/events';
+import MainNavigation from './components/navigation/mainNavigation';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
